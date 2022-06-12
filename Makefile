18807ccddb6a762a83977aa9f42da22e6dd521a2
Vinz2008 <vincentbidarddelanoe@gmail.com>
2022-06-12 19:17:02 +0200

    added removing from array

--- Makefile
+++ Makefile
@@ -6,6 +6,7 @@ all:
 	mkdir build
 	$(CC) -c -g libs/run_command.c -o build/run_command.o
 	$(CC) -c -g libs/open_file_in_str.c -o build/open_file_in_str.o
+	$(CC) -c -g libs/array.c -o build/array.o
 	$(CC) -c -g main.c -o build/main.o `pkg-config --cflags --libs gtk4 libvlc`
 	$(CC) -o audio-player build/main.o build/run_command.o build/open_file_in_str.o `pkg-config --cflags --libs gtk4 libvlc xkbcommon-x11 xcb`
 	rm -rf build
---
+++ libs/array.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char** append_to_array(char* thing, char** array){
+    char** tempArray = malloc(sizeof(array) + sizeof(thing));
+    int templength;
+    int iTemp = 0; 
+    while (array[iTemp]){
+        templength++;
+        iTemp++;
+    }
+    int end;
+    for (int i = 0; i < templength; i++){
+        strcpy(tempArray[i], array[i]);
+        end = i;
+    }
+    strcpy(tempArray[end + 1], thing);
+    return tempArray;
+}
+
+
+char** remove_from_array_last(char** array){
+    char** tempArray = malloc(sizeof(array) - sizeof(char*));
+    int templength;
+    int iTemp = 0; 
+    while (array[iTemp]){
+        templength++;
+        iTemp++;
+    }
+    for (int i = 0; i < templength - 1; i++){
+        strcpy(tempArray[i], array[i]);
+    }
+    return tempArray;
+}
+
+char** remove_from_array(char* thing, char** array){
+    int i;
+    int i2 = 0;
+    int pos;
+    char** arrayCopied;
+    char** tempArray;
+    int templength;
+    int iTemp = 0;
+    while (array[iTemp]){
+        templength++;
+        iTemp++;
+    }
+    for (i = 0; i < templength; i++){
+        strcpy(arrayCopied[i], array[i]);
+    }
+    for (i = 0; i < templength; i++){
+        if (arrayCopied[i] == thing){
+            pos = i;
+        }
+    }
+    for (i = 0; i < templength; i++){
+        if (i != pos){
+            strcpy(tempArray[i], arrayCopied[i2]);
+        }
+        i2++;
+    }
+    return tempArray;
+}
